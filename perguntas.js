206a64d96c123b713bdc0415c8498289db872390
Ana Pavani <142330742+aninha100@users.noreply.github.com>
2024-11-18 11:40:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+criaCartao(
+    'Programação',
+    'O que é lógica de programação?',
+    'É todo conjunto de regras e conceitos que precisa ser aplicado para criar códigos que serão interpretados e executados por um computador.'
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual a maior capital da Austria?',
+    'A maior capital da Austria é Sydney'
+)
+
+
+criaCartao(
+    'Programação',
+    'Quais são os 3 tipos de programação?',
+    'linguagens de baixo nível, linguagens de alto nível e linguagens de script.'
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz como você esta em Inglês?',
+    'Como você esta em inglês é How are you.'
+)
+
+criaCartao( 
+    'Matematica',
+    'Quanto é 5x5?',
+    ' A resposta é 25.'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Quem descobriu o Brasil foi Pedro Alvares Cabral.'
+)
+
+criaCartao( 
+    'futebol',
+    'Quem é o Rei do futebol no Brasil?',
+    'O Rei do futebol no Brasil é o Pelé.'
+)
+
+criaCartao( 
+    'Moda',
+    'Qual o nome da modelo mais famosa do Mundo',
+    ' A modelo mais famosa do Mundo é Kendall Jenner.'
+)
